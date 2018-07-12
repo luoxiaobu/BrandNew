dcc2116f64868f7419ac3da9919fbf86c6c496eb
luoxiaobu <luoxiaobu@users.noreply.github.com>
2018-07-13 00:34:35 +0800

    test change user

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,36 +1,37 @@
 //home.js
 //获取应用实例
-var postsData=require('../../data/posts-data.js')
+
+var postsData = require('../../data/posts-data.js')
 const app = getApp()
 
 Page({
-  data: {
-    img: [],
-    list: [],
-    indicatorDots: true,
-    vertical: false,
-    autoplay: false,
-    circular: false,
-    interval: 4000,
-    duration: 300
-  },
-  // //事件处理函数
-  onLoad: function() {
-    var postSwiper = [{
-      imgSrc: "/img/qixi1.jpg",
-      info: 'haha'
-    }, {
-      imgSrc: "/img/qixi2.jpg",
-      info: 'haha1'
-    }, {
-      imgSrc: "/img/qixi3.jpg",
-      info: 'haha1'
-    }];
-    this.setData({
-      img: postSwiper
-    });
-    this.setData({
-      list: postsData.postList
-    });
-  },
+    data: {
+        img: [],
+        list: [],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: false,
+        circular: false,
+        interval: 4000,
+        duration: 300
+    },
+    // //事件处理函数
+    onLoad: function() {
+        var postSwiper = [{
+            imgSrc: "/img/qixi1.jpg",
+            info: 'haha'
+        }, {
+            imgSrc: "/img/qixi2.jpg",
+            info: 'haha1'
+        }, {
+            imgSrc: "/img/qixi3.jpg",
+            info: 'haha1'
+        }];
+        this.setData({
+            img: postSwiper
+        });
+        this.setData({
+            list: postsData.postList
+        });
+    },
 })
