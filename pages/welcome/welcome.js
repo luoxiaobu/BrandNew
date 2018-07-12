6b7110a1faf6777b1bff7d2f2c8b4981b7c4b336
luoxiaobu <luoxiaoer@users.noreply.github.com>
2018-07-13 00:08:56 +0800

    format file

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto:'开启旅程',
+    motto: '开启旅程',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
@@ -15,13 +15,13 @@ Page({
       url: '../home/home'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
