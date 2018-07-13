93d7ae0658211f196997b06e8c063d7912c1ba8d
luoxiaobu <luoxiaobu@users.noreply.github.com>
2018-07-13 09:11:19 +0800

    add collected

--- data/posts-data.js
+++ data/posts-data.js
@@ -1,21 +1,23 @@
 var local_data = [{
     imgAuthor: "/img/avatar/1.jpg",
-    timeDate: '06/07/2018',
+    timeDate: '10/07/2018',
     title: '邪不压正',
     imgSrc: '/img/list/xiebu.jpg',
     comment: '北洋年间，北京以北。习武少年李天然（彭于晏饰）目睹师兄朱潜龙（廖凡饰）勾结日本特务山本一郎（泽田谦也饰），杀害师父全家。李天然侥幸从枪下逃脱，被美国医生亨德勒救下。李天然伤愈后，赴美学医多年，并同时接受特工训练。1937年初，李天然突然受命回国。“七七事变”前夜，北平，这座国际间谍之城，华洋混杂，山头林立，每时每刻充满诱惑与杀机。一心复仇的李天然，并不知道自己被卷入了一场阴谋，亦搅乱了一盘棋局。彼时彼刻，如同李小龙闯进了谍都卡萨布兰卡。前朝武人蓝青峰（姜文饰），神秘莫测，与朱潜龙、山本一郎关系紧密，更与亨德勒情同手足。是敌是友？面目不清。',
     reading: '112',
     collection: '96',
-    postId: 1
+    postId: 1,
+    collected: true,
 }, {
     imgAuthor: "/img/avatar/2.jpg",
-    timeDate: '07/07/2018',
+    timeDate: '09/07/2018',
     title: '加勒比海盗',
     imgSrc: '/img/list/jiale.jpg',
     comment: '系列电影包括《加勒比海盗：黑珍珠号的诅咒》、《加勒比海盗：聚魂棺》、《加勒比海盗：世界的尽头》、《加勒比海盗：惊涛怪浪》以及《加勒比海盗：死无对证》共5部。',
     reading: '10000',
     collection: '1000',
-    postId: 2
+    postId: 2,
+    collected: false,
 },{
     imgAuthor: "/img/avatar/3.jpg",
     timeDate: '08/07/2018',
@@ -25,6 +27,7 @@ var local_data = [{
     reading: '3004',
     collection: '500',
     postId: 3,
+    collected: false,
 }]
 module.exports = {
     postList: local_data
